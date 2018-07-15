cda08facb9e9e7298920d2089824861abc6a8d99
Beamanator <dabeamanator@gmail.com>
2018-07-15 12:41:18 +0200

    Adding form elements

--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -1,21 +1,37 @@
 import React, { Component, Fragment } from 'react';
 
 import {
-    Button,
+    Button, TextField,
     Dialog,
     DialogActions,
     DialogContent,
     DialogContentText,
-    DialogTitle
+    DialogTitle,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    withStyles
 } from '@material-ui/core';
 
 import {
     Add
 } from '@material-ui/icons';
 
-export default class extends Component {
+const styles = theme => ({
+    FormControl: {
+        width: 500
+    }
+})
+
+export default withStyles(styles)(class extends Component {
     state = {
-        open: false
+        open: false,
+        exercise: {
+            title: '',
+            description: '',
+            muscles: ''
+        }
     }
 
     toggleHandler = () => {
@@ -24,8 +40,43 @@ export default class extends Component {
         })
     }
 
+    handleChange = name => ({ target: {value } }) => {
+        this.setState({
+            exercise: {
+                ...this.state.exercise,
+                [name]: value
+            }
+        })
+    }
+
+    handleSubmit = (event) => {
+        // TODO: validations
+
+        const { exercise } = this.state;
+
+        this.props.onCreate({
+            ...exercise,
+            id: exercise.title.toLocaleLowerCase().replace(/ /g, '-')
+        });
+
+        this.setState({
+            open: false,
+            exercise: {
+                title: '',
+                description: '',
+                muscles: ''
+            }
+        })
+    }
+
     render() {
-        const { open } = this.state;
+        const {
+            open,
+            exercise: { title, description, muscles}
+        } = this.state, {
+            classes,
+            muscles: categories
+        } = this.props;
 
         return <Fragment>
             <Button variant="fab" onClick={this.toggleHandler} mini>
@@ -43,15 +94,52 @@ export default class extends Component {
                     <DialogContentText>
                         Please fill out the form below.
                     </DialogContentText>
-                    {/* <Form>
-                    </Form> */}
+                    <form>
+                        <TextField
+                            className={classes.FormControl}
+                            label="Title"
+                            value={title}
+                            onChange={this.handleChange('title')}
+                            margin="normal"
+                        />
+                        <br />
+                        <FormControl className={classes.FormControl}>
+                            <InputLabel htmlFor="muscles">
+                                Muscles
+                            </InputLabel>
+                            <Select
+                                value={muscles}
+                                onChange={this.handleChange('muscles')}
+                            >
+                                {categories.map(category =>
+                                    <MenuItem value={category} key={category}>
+                                        {category}
+                                    </MenuItem>
+                                )}
+                            </Select>
+                        </FormControl>
+                        <br />
+                        <TextField
+                            className={classes.FormControl}
+                            multiline
+                            rows="4"
+                            label="Description"
+                            value={description}
+                            onChange={this.handleChange('description')}
+                            margin="normal"
+                        />
+                    </form>
                 </DialogContent>
                 <DialogActions>
-                    <Button color="primary" variant="raised">
+                    <Button
+                        color="primary"
+                        onClick={this.handleSubmit}
+                        variant="raised"
+                    >
                         Create
                     </Button>
                 </DialogActions>
             </Dialog>
         </Fragment>
     }
-}
+})
